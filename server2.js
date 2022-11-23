f2040ca074e55b40966f256c1d6b3768a5ea774b
khushikukreti <khushikukreti20@gmail.com>
2022-11-24 01:32:34 +0530

    first commit!

--- server2.js
+++ server2.js
@@ -1,5 +1,7 @@
 
-const simpleGit = require("simple-git")();
+const simpleGit = require("simple-git");
+simpleGit().clean(simpleGit.CleanOptions.FORCE);
+
 // Shelljs package for running shell tasks optional
 const shellJs = require("shelljs");
 // Simple Git with Promise for handling success and failure
@@ -13,31 +15,37 @@ const userName = 'khushikukreti';
 const password = 'Pinki20#';
 // Set up GitHub url like this so no manual entry of user pass needed
 const gitHubUrl = `https://${userName}:${password}@github.com/${userName}/${repo}`;
-// add local git config like username and email
-simpleGit.addConfig('user.email','khushikukreti20@gmail.com');
-simpleGit.addConfig('user.name','khushikukreti');
+// // add local git config like username and email
+// simpleGit.addConfig('user.email','khushikukreti20@gmail.com');
+// simpleGit.addConfig('user.name','khushikukreti');
+simpleGit()
+  .init()
+  .add("./*")
+  .commit("first commit!")
+  .addRemote("origin", gitHubUrl)
+  .push("origin", "master");
 // Add remore repo url as origin to repo
 // simpleGitPromise.addRemote('origin',gitHubUrl);
 // Add all files for commit
-  simpleGitPromise.add('.')
-    .then(
-       (addSuccess) => {
-          console.log(addSuccess);
-       }, (failedAdd) => {
-          console.log('adding files failed');
-    });
-// Commit files as Initial Commit
- simpleGitPromise.commit('Intial commit by simplegit')
-   .then(
-      (successCommit) => {
-        console.log(successCommit);
-     }, (failed) => {
-        console.log('failed commmit');
- });
-// Finally push to online repository
- simpleGitPromise.push('origin','main')
-    .then((success) => {
-       console.log('repo successfully pushed');
-    },(failed)=> {
-       console.log('repo push failed');
- });
+//   simpleGitPromise.add('.')
+//     .then(
+//        (addSuccess) => {
+//           console.log(addSuccess);
+//        }, (failedAdd) => {
+//           console.log('adding files failed');
+//     });
+// // Commit files as Initial Commit
+//  simpleGitPromise.commit('Intial  by simplegit')
+//    .then(
+//       (successCommit) => {
+//         console.log(successCommit);
+//      }, (failed) => {
+//         console.log('failed commmit');
+//  });
+// // Finally push to online repository
+//  simpleGitPromise.push()
+//     .then((success) => {
+//        console.log('repo successfully pushed');
+//     },(failed)=> {
+//        console.log('repo push failed');
+//  });
