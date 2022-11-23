7e47ff7db8dcf8770eabb0dae2f2d52bad4e8e95
khushikukreti <khushikukreti20@gmail.com>
2022-11-24 01:22:44 +0530

    Intial commit by simplegit

--- server.js
+++ server.js
@@ -2,8 +2,20 @@
 const shell = require('shelljs')
 // shell.exec('./feature')
 const {exec}=require('node:child_process')
+// exec(
+//   'git branch feature \n git checkout feature \n git add . \n git commit -m "hi" \n git push --set-upstream origin feature',
+//   (error, stdout, stderr) => {
+//     if (error) {
+//       console.log(error);
+//     }
+//     if (stderr) {
+//       console.log(stderr);
+//     }
+//     console.log(stdout);
+//   }
+// );
 exec(
-  'git branch feature \n git checkout feature \n git add . \n git commit -m "hi" \n git push --set-upstream origin feature',
+  'git merge feature',
   (error, stdout, stderr) => {
     if (error) {
       console.log(error);
@@ -14,12 +26,3 @@ exec(
     console.log(stdout);
   }
 );
-// exec("git commit -m \"hi\"", (error, stdout, stderr) => {
-//   if (error) {
-//     console.log(error);
-//   }
-//   if (stderr) {
-//     console.log(stderr);
-//   }
-//   console.log(stdout);
-// });
