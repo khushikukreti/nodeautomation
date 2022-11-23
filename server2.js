fdc213495defea2fe0edae1d215fff4fdaac34fe
khushikukreti <khushikukreti20@gmail.com>
2022-11-24 01:39:30 +0530

    first commit!

--- server2.js
+++ server2.js
@@ -33,7 +33,7 @@ simpleGit()
     }
   );
    simpleGit()
-     .checkout("main","origin")
+     .checkoutBranch("main", "origin")
      .then(
        (success) => {
          console.log("repo checkout pushed!");
