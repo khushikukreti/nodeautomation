f4e67f66f31a0e9f8ced2b9b80719b6233cfdfef
khushikukreti <khushikukreti20@gmail.com>
2022-11-24 19:39:47 +0530

    y

---
+++ server3.js
@@ -0,0 +1,20 @@
+const simpleGit = require("simple-git");
+simpleGit().clean(simpleGit.CleanOptions.FORCE);
+const dotenv = require("dotenv").config();
+const shellJs = require("shelljs");
+const simpleGitPromise = require("simple-git")();
+const repo = "nodeautomation"; //Repo name
+console.log(process.env.NAME);
+const userName = process.env.NAME;
+const password = process.env.PASSWORD;
+const gitHubUrl = `https://${userName}:${password}@github.com/${userName}/${repo}`;
+simpleGit().addConfig("user.email", "khushikukreti20@gmail.com");
+simpleGit().addConfig("user.name", "khushikukreti");
+const add = async () => {
+  simpleGit().add("./*");
+};
+simpleGit()
+  .add(".")
+  .commit("first commit!")
+  .addRemote("origin", "some-repo-url")
+  .push(["-u", "origin", "main"], () => console.log("done"));
