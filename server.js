ac0277c9339bafa567337bf948be6087dbb0c6bb
khushikukreti <khushikukreti20@gmail.com>
2022-11-24 10:58:51 +0530

    first commit!

--- server.js
+++ server.js
@@ -2,20 +2,9 @@
 const shell = require('shelljs')
 // shell.exec('./feature')
 const {exec}=require('node:child_process')
-// exec(
-//   'git branch feature \n git checkout feature \n git add . \n git commit -m "hi" \n git push --set-upstream origin feature',
-//   (error, stdout, stderr) => {
-//     if (error) {
-//       console.log(error);
-//     }
-//     if (stderr) {
-//       console.log(stderr);
-//     }
-//     console.log(stdout);
-//   }
-// );
 exec(
-  'git merge feature',
+  'git checkout main',
+//   \n git add . \n git commit -m "hi" \n git push --set-upstream origin feature',
   (error, stdout, stderr) => {
     if (error) {
       console.log(error);
@@ -26,3 +15,15 @@ exec(
     console.log(stdout);
   }
 );
+// exec(
+//   'git merge feature',
+//   (error, stdout, stderr) => {
+//     if (error) {
+//       console.log(error);
+//     }
+//     if (stderr) {
+//       console.log(stderr);
+//     }
+//     console.log(stdout);
+//   }
+// );
