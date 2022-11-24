1777514888767bbcdcea182510038badb9b24673
khushikukreti <khushikukreti20@gmail.com>
2022-11-24 11:34:10 +0530

    hi

--- server.js
+++ server.js
@@ -2,21 +2,27 @@
 const shell = require('shelljs')
 // shell.exec('./feature')
 const {exec}=require('node:child_process')
-exec('git add . \n git commit -m "hi" \n git push', (error, stdout, stderr) => {
-  if (error) {
-    console.log(error);
+exec(
+  'git checkout main \n git add . \n git commit -m "hi" \n git push --set-upstream origin feature',
+  (error, stdout, stderr) => {
+    if (error) {
+      console.log(error);
+    }
+    if (stderr) {
+      console.log(stderr);
+    }
+    console.log(stdout);
   }
-  if (stderr) {
-    console.log(stderr);
-  }
-  console.log(stdout);
-});
-// exec("git commit -m \"hi\"", (error, stdout, stderr) => {
-//   if (error) {
-//     console.log(error);
+);
+// exec(
+//   'git merge feature',
+//   (error, stdout, stderr) => {
+//     if (error) {
+//       console.log(error);
+//     }
+//     if (stderr) {
+//       console.log(stderr);
+//     }
+//     console.log(stdout);
 //   }
-//   if (stderr) {
-//     console.log(stderr);
-//   }
-//   console.log(stdout);
-// });
+// );
