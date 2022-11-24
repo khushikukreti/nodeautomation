e19691bec73bbd5d24edf275e1a8a6f104cc14ac
khushikukreti <khushikukreti20@gmail.com>
2022-11-24 15:17:03 +0530

    h1

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ const shell = require('shelljs')
 // shell.exec('./feature')
 const {exec}=require('node:child_process')
 exec(
-  'git checkout main \n git add . \n git commit -m "hi" \n git push --set-upstream origin feature',
+  'git add . \n git commit -m "h1" \n git checkout main \n git add . \n git commit -m "hi" \n git push --set-upstream origin feature',
   (error, stdout, stderr) => {
     if (error) {
       console.log(error);
@@ -20,26 +20,10 @@ exec(
 //     if (error) {
 //       console.log(error);
 //     }
-//     if (stderr) {
-//       console.log(stderr);
-//     }
-//     console.log(stdout);
-//   }
-// );
-// let st=''
-// exec(
-//  "git diff --name-only ..origin/feature",
-//   (error, stdout, stderr) => {
-//     if (error) {
-//       console.log(error);
-//     }
-//     if (stderr) {
-//       console.log(stderr);
-//     }
-//     st=stdout
-//     console.log('st',st);
-//     console.log(stdout);
-//   }
-// );
-// console.log(st);
 
+//     if (stderr) {
+//       console.log(stderr);
+//     }
+//     console.log(stdout);
+//   }
+// );
