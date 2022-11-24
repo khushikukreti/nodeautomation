802ed92caa29acef7c623675783f893510daa416
khushikukreti <khushikukreti20@gmail.com>
2022-11-24 19:40:58 +0530

    first commit!

--- server3.js
+++ server3.js
@@ -17,4 +17,4 @@ simpleGit()
   .add(".")
   .commit("first commit!")
   .addRemote("origin", gitHubUrl)
-  .push(["-u", "origin", "feature"], () => console.log("done"));
+  .push("origin", "feature", () => console.log("done"));
